0aca77a81955c726b90dbd75e79a7c70c4e70d90
kento kadowaki <kadowakikento@gmail.com>
2022-06-16 01:24:34 +0900

    init

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -67,13 +67,12 @@ class PicturesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_picture
-      @picture = Picture.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def picture_params
+  def set_picture
+      @picture = Picture.find(params[:id])
+  end
+
+  def picture_params
       params.require(:picture).permit(:image, :image_cache, :content)
-    end
+  end
 end
